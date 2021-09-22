9de357d67c27c2a3b9650b4bf1750edd55cf6fb3
Patrick Nelson <pnelson5@gmail.com>
2021-09-22 11:44:47 -0400

    Sorted squares

---
+++ src/test/java/com/nelson/app/SortedSquaresTest.java
@@ -0,0 +1,13 @@
+package com.nelson.app;
+
+import org.junit.Test;
+
+public class SortedSquaresTest {
+    
+    @Test
+    public void test() {
+        SortedSquares app = new SortedSquares();
+        app.sortedSquares(new int[]{1,2,3});
+    }
+
+}
