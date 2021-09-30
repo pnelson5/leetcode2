4678ade00a3c86fec2e702654d2ae10fb1a6acea
Patrick Nelson <pnelson5@gmail.com>
2021-09-30 10:06:41 -0400

    remove nodes

--- src/main/java/com/nelson/app/RemoveNode.java
+++ src/main/java/com/nelson/app/RemoveNode.java
@@ -3,6 +3,25 @@ package com.nelson.app;
 public class RemoveNode {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode slow = new ListNode(-1, head);
+        ListNode fast = head;
+
+        for (int i = 1; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        if (slow.val == -1) {
+            return slow.next;
+        }
+
         return head;
     }
 
